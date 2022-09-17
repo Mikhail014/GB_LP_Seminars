b251c7589f5ab93a9fb20bb229683dc9cac3ac4f
Mikhail014 <msemin704@gmail.com>
2022-09-17 13:03:49 +0300

    Решение задачи 2 - ДЗ

---
+++ Task02_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Команда для изменения кодировки в терминале
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второго целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max){
+    max = num2;
+}
+
+Console.WriteLine($"Наибольшее число: {max}");
