8d0de4bdec3494baab7e3c8f70774554f7a112b4
Mikhail014 <msemin704@gmail.com>
2022-09-18 17:00:13 +0300

    Перемещение решений задач в папку Seminar 1

---
+++ Seminar 1/Task05/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+
+while (i <= num){
+    Console.Write($"{i++} ");
+}
---
+++ Seminar 1/Task08_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿// Команда для изменения кодировки в терминале
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+while (i <= num){
+    if (i == num || i == num - 1) Console.Write($"{i}");
+    else Console.Write($"{i}, ");
+    i += 2;
+}
