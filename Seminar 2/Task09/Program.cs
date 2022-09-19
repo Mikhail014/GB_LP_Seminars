5c142c3bbfbcc0e11e01af9d207e9b5a132973e9
Mikhail014 <msemin704@gmail.com>
2022-09-19 20:33:01 +0300

    Решение задачи 9

---
+++ Seminar 2/Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int randNum = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайные числа из отрезка 10 -> 99: {randNum}");
+
+int Digit1 = randNum / 10;
+int Digit2 = randNum % 10;
+
+if (Digit1 > Digit2) Console.WriteLine($"Наибольшая цифра числа: {Digit1}");
+else if (Digit1 == Digit2) Console.WriteLine("Цифры одинаковые");
+else Console.WriteLine($"Наибольшая цифра числа: {Digit2}");
