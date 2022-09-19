4f3b10ca0efc3febca8988a36ef119c8e9e6927e
Mikhail014 <msemin704@gmail.com>
2022-09-19 22:00:20 +0300

    Решение задачи 12

--- Seminar 2/Task012/Program.cs
+++ Seminar 2/Task012/Program.cs
@@ -1,2 +1,17 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Multiple(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+
+string result = Multiple(num1, num2) == 0 ? $"Числа {num1} и {num2} кратны" : $"Числа {num1} и {num2} некратны. Остаток: {num1 & num2}";
+
+Console.WriteLine(result);
