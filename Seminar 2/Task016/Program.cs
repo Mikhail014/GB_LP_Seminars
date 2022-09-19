50286cd2c71d313ce0984d20c8a9ee1f6d76b53a
Mikhail014 <msemin704@gmail.com>
2022-09-19 22:21:23 +0300

    Решение задачи 16

---
+++ Seminar 2/Task016/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool CheckQuad(int num1, int num2)
+{
+    return num1 * num1 == num2;
+}
+
+string result = CheckQuad(num1, num2) ? $"Число {num2} является квадратом числа {num1}" : $"Число {num2} не является квадратом числа {num1}";
+
+Console.WriteLine(result);
