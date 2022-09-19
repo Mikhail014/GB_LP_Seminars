6d1cc5ae0863c8df0ad4cc2030c1cc46caae280c
Mikhail014 <msemin704@gmail.com>
2022-09-19 21:27:28 +0300

    Решение задачи 11

---
+++ Seminar 2/Task010/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int randNum = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерированное число: {randNum}");
+
+int func1(int num)
+{
+    int Digit1 = (randNum / 100) * 10;
+    int Digit2 = randNum % 10; 
+    return Digit1 + Digit2;
+}
+
+Console.Write("Результат: ");
+Console.WriteLine(func1(randNum));
