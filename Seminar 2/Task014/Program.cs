6ea06db2a2375c49094326b1fb3eaf08005d9b3c
Mikhail014 <msemin704@gmail.com>
2022-09-19 22:10:11 +0300

    Решение задачи 14

---
+++ Seminar 2/Task014/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool CheckMultiple(int num)
+{
+    return num % 7 == 0 && num % 23 == 0 ? true : false; 
+}
+
+string result = CheckMultiple(num) ? $"Число {num} кратно одновременно 7 и 23" : $"Число {num} некратно одновременно 7 и 23";
+
+Console.WriteLine(result);
