f2f12cfb68301b61ee20ac5214049cfe1b6b460d
Mikhail014 <msemin704@gmail.com>
2022-09-19 20:45:07 +0300

    Создание функции MaxDigit

--- Seminar 2/Task09/Program.cs
+++ Seminar 2/Task09/Program.cs
@@ -2,14 +2,24 @@
 
 int randNum = new Random().Next(10, 100);
 
-Console.WriteLine($"Случайные числа из отрезка 10 -> 99: {randNum}");
 
-int Digit1 = randNum / 10;
-int Digit2 = randNum % 10;
 
-if (Digit1 > Digit2) Console.WriteLine($"Наибольшая цифра числа: {Digit1}");
-else if (Digit1 == Digit2) Console.WriteLine("Цифры одинаковые");
-else Console.WriteLine($"Наибольшая цифра числа: {Digit2}");
+
+// if (Digit1 > Digit2) Console.WriteLine($"Наибольшая цифра числа: {Digit1}");
+// else if (Digit1 == Digit2) Console.WriteLine("Цифры одинаковые");
+// else Console.WriteLine($"Наибольшая цифра числа: {Digit2}");
 
 
 // int max = Digit1 > Digit2 ? Digit1 : Digit2; // тернарное выражение
+
+int MaxDigit(int num)
+{
+    Console.WriteLine($"Случайные числа из отрезка 10 -> 99: {num}");
+
+    int Digit1 = num / 10;
+    int Digit2 = num % 10;
+
+    return Digit1 > Digit2 ? Digit1 : Digit2;
+}
+
+Console.WriteLine(MaxDigit(randNum));
