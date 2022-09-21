d96ca50069a7b828c5d6410ff19b85ee78d3b206
Mikhail014 <msemin704@gmail.com>
2022-09-21 10:08:00 +0300

    Решение задачи 10

---
+++ Seminar 2/Task010_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SecondNum(int num)
+{
+    if (num < 100 || num > 999) return -1;
+    return Math.Abs((num / 10) % 10);
+}
+
+if (SecondNum(num) != -1){
+    Console.WriteLine($"Вторая цифра в трехзначном числе: {SecondNum(num)}");
+}
+else
+{
+    Console.WriteLine("Ошибка! Введите трехзначное число!");
+}
