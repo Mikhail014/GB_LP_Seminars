f551b32dc4a0008aaa7c5df2381d4c09e82b2cff
Mikhail014 <msemin704@gmail.com>
2022-09-21 13:42:49 +0300

    Решение задачи 13

---
+++ Seminar 2/Task013_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThirdNumber(int num)
+{
+    num = Math.Abs(num);
+    if (num < 100) return -1;
+    while (num >= 1000)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+
+Console.WriteLine(ThirdNumber(number) == -1 ? "Третьей цифры нет" : $"Третье число: {ThirdNumber(number)}");
