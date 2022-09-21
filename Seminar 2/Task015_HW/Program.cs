2d6f0b7b1a50b903f592a713c493c44f7f2fbd4f
Mikhail014 <msemin704@gmail.com>
2022-09-21 14:34:17 +0300

    Решение задачи 15

---
+++ Seminar 2/Task015_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число от 1 до 7 включительно: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsDayOff(int num)
+{
+    return num > 5 && num < 8;
+}
+
+if (IsDayOff(number))
+{
+    Console.WriteLine($"Число {number}. Этот день является выходным!");
+}
+else
+{
+    Console.WriteLine(number < 1 || number > 7 ? "Пожалуйста, введите число от 1 до 7 включительно!" : $"Число {number}. Это будний день!");
+}
