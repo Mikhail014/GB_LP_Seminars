cedbeb28bf9cd8dd8846397c7a108c0b732e2ba8
Mikhail014 <msemin704@gmail.com>
2022-09-24 01:30:56 +0300

    Решение задачи 22

---
+++ Seminar3/Task022/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string GetTableQuad(int n)
+{
+    if (n <= 0) return "Введите положительное число!!!";
+    int i = 1;
+    string res = "\nТаблица квадратов:\n";
+    while (i <= n)
+    {
+        res += $"{i,3} {Math.Pow(i, 2).ToString(), 3}\n";
+        i++;
+    }
+    return res;
+}
+
+Console.WriteLine(GetTableQuad(num));
