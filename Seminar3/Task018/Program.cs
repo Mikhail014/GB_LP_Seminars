0447efa0c1d5bef5521496518ab4b2b2f44e8bbc
Mikhail014 <msemin704@gmail.com>
2022-09-24 00:11:35 +0300

    Решение задачи 18

---
+++ Seminar3/Task018/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите четверть: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string GetCoords(int q)
+{
+    if (q == 1) return "X меньше нуля, Y больше нуля";
+    if (q == 2) return "X больше нуля, Y больше нуля";
+    if (q == 3) return "X меньше нуля, Y меньше нуля";
+    if (q == 4) return "X больше нуля, Y меньше нуля";
+    return "Введите число от 1 до 4 включительно!!!";
+}
+
+string result = GetCoords(quarter);
+
+Console.WriteLine(quarter < 2 || quarter > 4 ? result : $"Диапазон возможных координат: {result}");
