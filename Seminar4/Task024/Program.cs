6d18586883336f20bfba87a219197b2c0df49c0c
Mikhail014 <msemin704@gmail.com>
2022-09-26 20:38:26 +0300

    Задача 24

---
+++ Seminar4/Task024/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNums(int n)
+{
+    int sum = default;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+System.Console.WriteLine($"Сумма чисел от 1 до {num} равна {SumOfNums(num)}");
