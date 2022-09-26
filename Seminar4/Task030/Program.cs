54740b32f404e4c59f186a48ec5b681f99f4817b
Mikhail014 <msemin704@gmail.com>
2022-09-26 22:06:18 +0300

    Задача 30

---
+++ Seminar4/Task030/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[] GetArrayBin()
+{
+    int[] binNums = new int[8];
+    for (int i = 0; i < binNums.Length; i++)
+    {
+        binNums[i] = new Random().Next(0, 2);
+    }
+    return binNums;
+}
+
+int[] arrayNums = GetArrayBin();
+string res = default;
+int length = arrayNums.Length;
+
+for (int i = 0; i < arrayNums.Length; i++)
+{  
+    if (i == 0) res += "[";
+    if (i < length - 1) res += $"{arrayNums[i]}, ";
+    else res += $"{arrayNums[length - 1]}]";
+}
+
+System.Console.WriteLine(res);
