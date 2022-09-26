89f8df55466b2831484f6cfa7741c9c89b0e8a62
Mikhail014 <msemin704@gmail.com>
2022-09-26 20:54:14 +0300

    Задача 26

---
+++ Seminar4/Task026/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumberOfDigits(int n)
+{
+    int count = 0;
+    if (count == 0 && n == 0) return 1;
+    while (n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine(NumberOfDigits(num));
