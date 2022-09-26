356391808305ef3bf7c0f7f4a2e3aab1143077bd
Mikhail014 <msemin704@gmail.com>
2022-09-26 21:25:54 +0300

    Задача 28

---
+++ Seminar4/Task028/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int n)
+{
+    if (n == 0) return 1;
+    if (n < 0) return -1;
+    int f = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int res = Factorial(num);
+
+System.Console.WriteLine(res != -1 ? $"Факториал числа {num} равен {res}" : "Ошибка! Введенное число должно быть больше, либо равно нулю!");
