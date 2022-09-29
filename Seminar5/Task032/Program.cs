894b5fac587e99a06d35918e800fb85fdbf82c21
Mikhail014 <msemin704@gmail.com>
2022-09-29 21:22:06 +0300

    Задача 32. Добавлен ввод чисел

--- Seminar5/Task032/Program.cs
+++ Seminar5/Task032/Program.cs
@@ -1,4 +1,26 @@
-﻿int[] GetInversionArray(int[] arr)
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число меньше нуля: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число больше нуля: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRandomNums(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random r = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = r.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] GetInversionArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -7,7 +29,8 @@
     return arr;
 }
 
-int[] arrayNums = GetInversionArray(new int[]{-4, -8, 4, 8});
+int[] arrayNums = CreateArrayRandomNums(size, min, max);
+int[] inversionArray = GetInversionArray(arrayNums);
 
 void PrintArray(int[] arr)
 {
@@ -21,4 +44,4 @@ void PrintArray(int[] arr)
 }
 
 System.Console.WriteLine("Инверсия массива:");
-PrintArray(arrayNums);
+PrintArray(inversionArray);
