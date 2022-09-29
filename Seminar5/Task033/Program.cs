ddc8f7c03f69c806c846b48c3a515274351a1872
Mikhail014 <msemin704@gmail.com>
2022-09-29 21:07:48 +0300

    Задача 33

---
+++ Seminar5/Task033/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool IsElementInArray(int n, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == n) return true;
+    }
+    return false;
+}
+
+bool res = IsElementInArray(num, new int[]{2, 5, 3, 2});
+
+if (res) Console.WriteLine($"Число {num} присуствует в массиве!");
+else Console.WriteLine($"Число {num} не присуствует в массиве!");
