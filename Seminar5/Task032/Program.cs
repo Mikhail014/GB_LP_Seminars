c227644eca4a2ae9210c247d19fed15d08b79d22
Mikhail014 <msemin704@gmail.com>
2022-09-29 20:54:12 +0300

    Задача 32

---
+++ Seminar5/Task032/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] GetInversionArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+
+int[] arrayNums = GetInversionArray(new int[]{-4, -8, 4, 8});
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray($"Инверсия массива: {arrayNums}");
