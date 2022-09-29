30947c2efeee808653ae6017df364d3ef1c1d4d9
Mikhail014 <msemin704@gmail.com>
2022-09-29 21:35:37 +0300

    Задача 33. Добавлен ввод чисел, метод создания и печати массива

--- Seminar5/Task033/Program.cs
+++ Seminar5/Task033/Program.cs
@@ -1,7 +1,27 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.Write("Введите число: ");
+Console.Write("Количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите любое целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRandomNums(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random r = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = r.Next(min, max + 1);
+    }
+    return array;
+}
 
 bool IsElementInArray(int n, int[] arr)
 {
@@ -12,7 +32,22 @@ bool IsElementInArray(int n, int[] arr)
     return false;
 }
 
-bool res = IsElementInArray(num, new int[]{2, 5, 3, 2});
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRandomNums(size, min, max);
+bool res = IsElementInArray(num, arr);
+
+Console.WriteLine("Массив:");
+PrintArray(arr);
 
 if (res) Console.WriteLine($"Число {num} присуствует в массиве!");
 else Console.WriteLine($"Число {num} не присуствует в массиве!");
