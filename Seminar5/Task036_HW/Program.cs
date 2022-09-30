4cf85e88cdb5909d03c10d7096304de0546bae52
Mikhail014 <msemin704@gmail.com>
2022-09-30 10:41:44 +0300

    Задача 36. Домашнее задание

---
+++ Seminar5/Task036_HW/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRandomNums(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random r = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = r.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumElemOddIndex(int[] arr)
+{
+    int sum = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArrayRandomNums(size, min, max);
+int res = GetSumElemOddIndex(arr);
+
+Console.Write("Массив: ");
+PrintArray(arr);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {res}");
