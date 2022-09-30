0ba37d45c152c830c18d48a6d2fce84140addfe3
Mikhail014 <msemin704@gmail.com>
2022-09-30 10:31:17 +0300

    Задача 34. Домашнее задание

---
+++ Seminar5/Task034_HW/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRandomNums(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random r = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = r.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GetAmountEvenNums(int[] arr)
+{
+    int res = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) res++;
+    }
+    return res;
+}
+
+int[] arr = CreateArrayRandomNums(size, 100, 999);
+int evenNums = GetAmountEvenNums(arr);
+
+Console.Write("Массив: ");
+PrintArray(arr);
+Console.WriteLine($"Количество четных чисел в массиве: {evenNums}");
