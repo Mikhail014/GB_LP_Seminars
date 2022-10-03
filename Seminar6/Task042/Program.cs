d5bef3dfd9ab0d9a9b303ee1944409742c7730bf
Mikhail014 <msemin704@gmail.com>
2022-10-03 21:25:02 +0300

    Задача 42

---
+++ Seminar6/Task042/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string ConvertBinNum(int n)
+{
+    string str = default;
+    while (n != 0)
+    {
+        str += (n % 2).ToString();
+        n /= 2;
+    }
+    char[] chars = str.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
+
+System.Console.WriteLine(ConvertBinNum(num));
