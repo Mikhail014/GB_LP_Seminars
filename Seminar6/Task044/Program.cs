65233c0285c0c7b97d8bda1ea4443f5ddaf89085
Mikhail014 <msemin704@gmail.com>
2022-10-04 00:25:41 +0300

    Задача 44

---
+++ Seminar6/Task044/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void PrintFibonacci(int n)
+{
+
+    int num1 = default;
+    int num2 = default;
+    int f = default;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (i == 0) 
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+        else if (i == 1 || i == 2)
+        {
+            num1 = 0;
+            num2 = 1;
+        }
+        else
+        {
+            num1 = num2;
+            num2 = f;
+        }
+        
+        
+        f = num1 + num2;
+        Console.Write($"{f} ");
+        
+    }
+}
+
+PrintFibonacci(num);
