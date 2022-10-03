72dcf510c92db2e7ec1571b10e19c2818e8b3195
Mikhail014 <msemin704@gmail.com>
2022-10-03 20:46:59 +0300

    Задача 40

---
+++ Seminar6/Task040/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Первая сторона треугольника: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Вторая сторона треугольника: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Третья сторона треугольника: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool IsCheckTriangle(int n1, int n2, int n3)
+{
+    if (n1 < n2 + n3 && n2 < n1 + n2 && n3 < n1 + n2) return true;
+    return false; 
+}
+
+bool res = IsCheckTriangle(num1, num2, num3);
+
+System.Console.WriteLine(res ? "Треугольник может существовать" : "Треугольник не может существовать");
