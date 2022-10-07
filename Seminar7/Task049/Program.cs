ad1247fe0e6b79b1afae3e806d17c6d0f9d45052
Mikhail014 <msemin704@gmail.com>
2022-10-08 01:50:59 +0300

    Задача 49

---
+++ Seminar7/Task049/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Размер двумерного массива");
+
+Console.Write("Количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] ReplaceEvenIndexMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random r = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            int num = r.Next(min, max + 1);
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] = num * num;
+            else matrix[i, j] = num;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixInt(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j]} |");
+            else Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] matrix = ReplaceEvenIndexMatrix(rows, columns, min, max);
+PrintMatrixInt(matrix);
