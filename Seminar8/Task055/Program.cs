4185f296f6ff2af9c40b1f67c540fb879f73d98b
Mikhail014 <msemin704@gmail.com>
2022-10-10 20:48:49 +0300

    Задача 55

---
+++ Seminar8/Task055/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Размер двумерного массива");
+
+Console.Write("Количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRandInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random r = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = r.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j]} |");
+            else Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] ReplacingRowsWithColumns(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+    if (rows != cols) return new int[0,0];
+    int[,] res = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            res[i, j] = matrix[j, i];
+        }
+    }
+    return res;
+}
+
+int[,] matrix = CreateMatrixRandInt(rows, columns, min, max);
+int[,] res = ReplacingRowsWithColumns(matrix);
+
+if (res.GetLength(0) == 0 && res.GetLength(1) == 0)
+{
+    System.Console.WriteLine("\nНужна квадратная матрица!\n");
+}
+else{
+    System.Console.WriteLine("\nМатрица:");
+    PrintMatrix(matrix);
+    System.Console.WriteLine("\nНовая матрица:");
+    PrintMatrix(res);
+}
