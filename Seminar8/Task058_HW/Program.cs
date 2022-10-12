787ba8d1f6762caa17f1bf7571a4656eb4b0399c
Mikhail014 <msemin704@gmail.com>
2022-10-12 04:34:25 +0300

    Задача 58. Домашнее задание

---
+++ Seminar8/Task058_HW/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Размер двумерного массива");
+
+Console.Write("Количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRandInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random r = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = r.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j]} |");
+            else Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] GetMulTwoMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int rows = matrix1.GetLength(0);
+    int cols = matrix1.GetLength(1);
+    int len = rows > cols ? cols : rows;
+    int[,] res = new int[rows,cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            for (int k = 0; k < len; k++)
+            {
+                res[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return res;
+}
+
+int[,] matrix1 = CreateMatrixRandInt(rows, columns, min, max);
+System.Console.WriteLine("\nМатрица 1:");
+PrintMatrix(matrix1);
+
+int[,] matrix2 = CreateMatrixRandInt(rows, columns, min, max);
+System.Console.WriteLine("\nМатрица 2:");
+PrintMatrix(matrix2);
+
+int[,] res = GetMulTwoMatrix(matrix1, matrix2);
+System.Console.WriteLine("\nПроизведение двух матриц:");
+PrintMatrix(res);
