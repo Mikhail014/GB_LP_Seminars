e3d0056e70784ed2e98fb93856662de6237dfd74
Mikhail014 <msemin704@gmail.com>
2022-10-13 23:11:36 +0300

    Задача 67

---
+++ Seminar9/Task067/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumOfNums(int n)
+{
+    int sum = default;
+    while(n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int res = GetSumOfNums(num);
+
+System.Console.WriteLine(res);
