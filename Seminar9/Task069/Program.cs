7f541f1bcdea197b5d10efd6cdfd06100f94a626
Mikhail014 <msemin704@gmail.com>
2022-10-13 23:21:01 +0300

    Задача 69

---
+++ Seminar9/Task069/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int RaiseNumberToPower(int num, int degree)
+{
+    if (degree == 0) return 1;
+    return num * RaiseNumberToPower(num, degree - 1);
+}
+
+int res = RaiseNumberToPower(n1, n2);
+System.Console.WriteLine($"Число {n1} в степени {n2} = {res}");
