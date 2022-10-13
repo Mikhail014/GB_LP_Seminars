11b9a64493658c2017d8ee16cf4fdffbdd5d2e6c
Mikhail014 <msemin704@gmail.com>
2022-10-13 23:02:31 +0300

    Задача 65

---
+++ Seminar9/Task065/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void GetNaturalNumbersFromMinToMax(int n, int m)
+{
+    if (m > n)
+    {
+        GetNaturalNumbersFromMinToMax(n, m - 1);
+        Console.Write($"{m} ");
+    }
+    if (m < n)
+    {
+        Console.Write($"{n} ");
+        GetNaturalNumbersFromMinToMax(n - 1, m);
+    }
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+    }
+}
+
+GetNaturalNumbersFromMinToMax(n, m);
