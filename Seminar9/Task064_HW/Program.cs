ee1e117b4bea0c1436751e34effbd52dcf45882e
Mikhail014 <msemin704@gmail.com>
2022-10-14 18:11:09 +0300

    Задача 64. Условие задачи и настройка вывода. Домашнее задание

--- Seminar9/Task064_HW/Program.cs
+++ Seminar9/Task064_HW/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -10,4 +16,5 @@ void GetNaturalNumbers(int n)
     GetNaturalNumbers(n - 1);
 }
 
+Console.Write("Вывод: ");
 GetNaturalNumbers(num);
