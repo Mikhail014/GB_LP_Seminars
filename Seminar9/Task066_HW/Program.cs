e3a09e0f462c6ae8651e4cf88434967e5da08a1d
Mikhail014 <msemin704@gmail.com>
2022-10-14 18:14:02 +0300

    Задача 66. Условие задачи. Домашнее задание

--- Seminar9/Task066_HW/Program.cs
+++ Seminar9/Task066_HW/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.Write("Введите число 1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
