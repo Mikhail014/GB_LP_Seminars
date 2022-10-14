997efcf3d60b9f1df18770acf98858323c11207a
Mikhail014 <msemin704@gmail.com>
2022-10-14 17:23:15 +0300

    Задача 64. Домашнее задание

---
+++ Seminar9/Task064_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void GetNaturalNumbers(int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    GetNaturalNumbers(n - 1);
+}
+
+GetNaturalNumbers(num);
