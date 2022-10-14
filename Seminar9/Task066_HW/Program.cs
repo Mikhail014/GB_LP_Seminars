9862328e74ded268d030354a1e5acf0318cf19d8
Mikhail014 <msemin704@gmail.com>
2022-10-14 17:45:04 +0300

    Задача 66. Домашнее задание

---
+++ Seminar9/Task066_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int GetSumOfNaturalNumbers(int n1, int n2)
+{
+    if (n2 > n1) return n2 + GetSumOfNaturalNumbers(n1, n2 - 1);
+    if (n2 < n1) return n1 + GetSumOfNaturalNumbers(n1 - 1, n2);
+    return n1;
+}
+
+int res = GetSumOfNaturalNumbers(num1, num2);
+System.Console.WriteLine($"Сумма натуральных чисел в промежутке от {num1} до {num2} равна {res}");
